bf3c2dabfecbb42c2035bbc055d089ad08e08499
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-22 08:47:54 -0500

    Un reloj con ReactJS

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,2 +1,14 @@
-const el = <h1 id="title">Hola <span>mundo</span></h1>
+const el = (
+	<h1 id="title">
+		Hola <span className="rojo">mundo</span> {2 + 5}
+	</h1>
+)
 ReactDOM.render(el, document.getElementById('app2'));
+
+function reloj(){
+	let now = new Date().toLocaleTimeString()
+	const el = <span>{now}</span>
+	ReactDOM.render(el, document.getElementById('reloj'))
+}
+
+setInterval(reloj, 1000)
